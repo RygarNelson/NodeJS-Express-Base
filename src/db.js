8f74e6a0f3e150eaba0c49ac8388393bbeea6e78
Daniele Moscati <d.moscati@nbsgroup.it>
2022-01-04 16:41:14 +0100

    Added support for PostgreSQL databases

--- src/db.js
+++ src/db.js
@@ -1,9 +1,28 @@
-var mysql = require('mysql');
+var mysql = require('mysql')
+var postgres = require('pg')
 
-var connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+var connection = null;
+
+switch (process.env.DB_TYPE) {
+    case 'mysql' : {
+        connection = mysql.createPool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+        })
+        break
+    }
+    case 'postgres' : {
+        connection = new postgres.Pool({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        })
+    }
+}
+
 module.exports = { connection }
