e7272d14056a6aa73a895d3ca768f5ce2b57d190
Daniele Moscati <d.moscati@nbsgroup.it>
2022-01-04 16:49:23 +0100

    Added sample route for checking database connection

--- src/routes/sample.js
+++ src/routes/sample.js
@@ -4,10 +4,19 @@ const express = require('express')
 
 const router = express.Router()
 
+const standardCallback = require('../methods/standardCallback')
+const { connection } = require('../db')
+
 router.get('/', async (req, res) => {
     res.status(200).send({
         success: true,
         data: "Connection with server established successfully"
+    })
+})
+
+router.get('/database', async (req, res) =>{
+    connection.query('select 1', (err, result) => {
+        standardCallback.execute(res, err, result)
     })
 })
 
